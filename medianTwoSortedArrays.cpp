8b21892cc83fdce61cbbcbea0882bf848add512d
GhostByDaylight <91904338+GhostByDaylight@users.noreply.github.com>
2022-07-11 14:03:53 -0400

    Update medianTwoSortedArrays.cpp

--- medianTwoSortedArrays.cpp
+++ medianTwoSortedArrays.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        //Merge the array -> nums3
+        //Merge the array
         vector<int> nums3;
         merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), back_inserter(nums3));
-      
-        // Test if there is an even or odd amount of elements in the array
-      
-        //Array has odd amount of elements
         if (nums3.size() % 2 != 0) {
-            //Find middle element -> index
             int index = (nums3.size()) / 2;
             return nums3[index];
         }
-        //Array has even amount of elements
         else if (nums3.size() % 2 == 0) {
-            //Find middle 2 elements, calculate the middle of them -> result
-            double result = (nums3[nums3.size() / 2] + nums3[(nums3.size() / 2) - 1]);
+            int firstMedian = nums3[nums3.size() / 2], lastMedian = nums3[(nums3.size() / 2) - 1];
+            double result = firstMedian + lastMedian;
             return result / 2;
         }
         else {
