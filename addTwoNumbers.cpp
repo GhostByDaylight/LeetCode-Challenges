5b538c0ed150386ab66551d4451263c5ad0714b3
GhostByDaylight <91904338+GhostByDaylight@users.noreply.github.com>
2022-07-11 14:38:20 -0400

    Update addTwoNumbers.cpp

--- addTwoNumbers.cpp
+++ addTwoNumbers.cpp
@@ -11,38 +11,19 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int l1val = 0, l2val = 0;
-        ListNode* l5 = new ListNode();
-        int multiplyer = 1;
-        
-        while (l1 != nullptr) {
-            l1val += l1->val * multiplyer;
-            multiplyer = multiplyer * 10;
-            l1 = l1->next;
+        ListNode* head = new ListNode();
+        ListNode* l3 = head; 
+        int extra = 0;
+        while (l1 != NULL || l2 != NULL || extra != 0) {
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int sum = extra + x + y;
+            extra = sum / 10;
+            l3->next = new ListNode(sum % 10);
+            l3 = l3->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
-        multiplyer = 1;
-        while (l2 != nullptr) {
-            l2val += l2->val * multiplyer;
-            multiplyer = multiplyer * 10;
-            l2 = l2->next;
-        }
-        int test = l1val + l2val;
-        ListNode* head = l5;
-        while (test != 0) {
-            ListNode* new_node = new ListNode();
-            l5->val = test % 10;
-            cout << "The value inserted in l5: " << test % 10 << endl << l5->val << endl;
-            l5->next = new_node;
-            test = test / 10;
-        }
-        l5 = head;
-        while (l5 != nullptr) {
-            cout << l5->val << endl;
-             l5 = l5->next;
-        }
-        cout << l1val << endl;
-        cout << l2val << endl;
-        
-        return l5;
+        return head->next;
     }
 };
